616ee88387e673394f914b3fe7953e8d3b958c74
ajea <smithajea@rocketmail.com>
2024-07-05 16:30:31 -0400

    for tes

--- app/api/folders/route.js
+++ app/api/folders/route.js
@@ -6,12 +6,10 @@ export async function GET() {
 	try {
 		await connectToDB();
 		const folders = await Folder.find({}).sort({ createdAt: "desc" }).lean();
-		console.log("Fetched folders:", folders); // Add logging
+		console.log("Fetched folders ==>", folders); // Add logging
 		return NextResponse.json(folders, {
 			status: 200,
-			headers: {
-				"Cache-Control": "no-store", // Ensure no caching
-			},
+			
 		});
 	} catch (error) {
 		console.error("Failed to fetch folders", error);
