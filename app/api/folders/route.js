de92a64ef3ac451f3e48b6e7853e27d08c21ddd9
ajea <smithajea@rocketmail.com>
2024-07-05 16:34:09 -0400

    added request to params

--- app/api/folders/route.js
+++ app/api/folders/route.js
@@ -2,14 +2,13 @@ import Folder from "@/lib/models/folder.model";
 import { connectToDB } from "@/lib/mongoose";
 import { NextResponse } from "next/server";
 
-export async function GET() {
+export async function GET(request) {
 	try {
 		await connectToDB();
 		const folders = await Folder.find({}).sort({ createdAt: "desc" }).lean();
 		console.log("Fetched folders ==>", folders); // Add logging
 		return NextResponse.json(folders, {
 			status: 200,
-			
 		});
 	} catch (error) {
 		console.error("Failed to fetch folders", error);
