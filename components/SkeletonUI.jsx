0c3ec0721344f38ff95b79d98721b7b22ded32b5
ajea <smithajea@rocketmail.com>
2024-07-05 18:33:36 -0400

    made skeleton ui client

--- components/SkeletonUI.jsx
+++ components/SkeletonUI.jsx
@@ -1,13 +1,11 @@
-'use client'
+"use client";
 
 export default function SkeletonUI() {
 	return (
 		<div className="mt-5 shadow rounded-md p-3 max-w-sm w-full mx-auto">
 			<div className="animate-pulse flex space-x-3 items-center">
 				<div className="rounded-full bg-slate-700 h-[8px] w-[8px]"></div>
-				<div className="flex-1 space-y-6 py-1">
-					<div className="h-2 bg-slate-700 rounded"></div>
-				</div>
+				<div className="flex-1 h-4 bg-slate-700 rounded"></div>
 			</div>
 		</div>
 	);
